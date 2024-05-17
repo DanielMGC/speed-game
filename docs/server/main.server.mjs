5f057889003876f7390389cf0350f721c639f13e
Daniel Moises Gonzalez Clua <daniel.moises.gc@gmail.com>
2024-05-17 10:30:04 -0400

    commit

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LKSOZFXY.mjs";import"./chunk-4JKD6LAF.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-6NOPD23M.mjs";import"./chunk-4JKD6LAF.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
