82350c4ed9475ee278bf4f25709d4149f092e5c2
Daniel Moises Gonzalez Clua <daniel.moises.gc@gmail.com>
2024-05-24 15:15:40 -0400

    changes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-A7PZC25Q.mjs";import"./chunk-T7K75PYA.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-5VJ4WE4T.mjs";import"./chunk-T7K75PYA.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
