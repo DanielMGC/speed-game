87ca1c197c5f94b7b08013f614239ca0510d71c1
Daniel Moises Gonzalez Clua <daniel.moises.gc@gmail.com>
2024-05-24 14:50:08 -0400

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,6 @@ export const routes: Routes = [
     { path: '', redirectTo: '/speed', pathMatch: 'full' },
     { path: 'speed', component: SpeedMainComponent },
     { path: 'speed/:score/:mode', component: SpeedMainComponent },
+    { path: 'browser/speed/:score/:mode', component: SpeedMainComponent },
+    { path: 'speed-game/browser/speed/:score/:mode', component: SpeedMainComponent },
   ];
